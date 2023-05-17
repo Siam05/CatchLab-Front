ea99035d0db5f369599829e78c44bb9eff2dbd20
Siam05 <58203992+Siam05@users.noreply.github.com>
2023-05-17 16:23:54 +0600

    V1

--- src/components/Home/Company-Overview.vue
+++ src/components/Home/Company-Overview.vue
@@ -4,16 +4,16 @@
             <div class="flex grid grid-cols-4 w-full">
                 <div class="flex flex-col items-center">
                     <img class="h-16" src="../../assets/icons/letter.png" alt="">
-                    <p class="text-5xl font-semibold pt-4 pb-2">1200k+</p>
+                    <p class="text-5xl font-semibold pt-4 pb-2">1200+</p>
                     <p class="text-sm">Completed Projects</p>
                 </div>
                 <div class="border-x border-background flex flex-col items-center">
                     <img class="h-16" src="../../assets/icons/people.png" alt="">
-                    <p class="text-5xl font-semibold pt-4 pb-2">120k+</p>
+                    <p class="text-5xl font-semibold pt-4 pb-2">120+</p>
                     <p class="text-sm">Happy Customer</p>
                 </div>
                 <div class="border-r border-background flex flex-col items-center">
-                    <img class="h-16" src="../../assets/icons/trophy.png" alt="">
+                    <img class="h-16" src="../../assets/icons/earth-grid.png" alt="">
                     <p class="text-5xl font-semibold pt-4 pb-2">14+</p>
                     <p class="text-sm">Countries Served</p>
                 </div>
@@ -28,22 +28,22 @@
        <div class="my-2 flex lg:hidden container grid grid-cols-1 gap-6 px-4 xl:px-20">
             <div class="border-b border-background pb-4 flex flex-col items-center">
                 <img class="h-12" src="../../assets/icons/letter.png" alt="">
-                <p class="text-3xl font-semibold pt-4 pb-2">3k+</p>
+                <p class="text-3xl font-semibold pt-4 pb-2">1200+</p>
                 <p class="text-sm">Completed Projects</p>
             </div>
             <div class="border-b border-background pb-4 flex flex-col items-center">
                 <img class="h-12" src="../../assets/icons/people.png" alt="">
-                <p class="text-3xl font-semibold pt-4 pb-2">20k+</p>
+                <p class="text-3xl font-semibold pt-4 pb-2">120+</p>
                 <p class="text-sm">Happy Customer</p>
             </div>
             <div class="border-b border-background pb-4 flex flex-col items-center">
-                <img class="h-12" src="../../assets/icons/trophy.png" alt="">
-                <p class="text-3xl font-semibold pt-4 pb-2">13+</p>
-                <p class="text-sm">Award Winning</p>
+                <img class="h-12" src="../../assets/icons/earth-grid.png" alt="">
+                <p class="text-3xl font-semibold pt-4 pb-2">14+</p>
+                <p class="text-sm">Countries Served</p>
             </div>
             <div class="flex flex-col items-center">
                 <img class="h-12" src="../../assets/icons/rating.png" alt="">
-                <p class="text-3xl font-semibold pt-4 pb-2">99%</p>
+                <p class="text-3xl font-semibold pt-4 pb-2">100%</p>
                 <p class="text-sm">Satisfaction Rate</p>
             </div>
        </div>
--- src/components/Home/Slider.vue
+++ src/components/Home/Slider.vue
@@ -2,7 +2,7 @@
     <div class="imageStyle">
         <div class="background-color h-full">
             <div class="h-full w-full flex flex-col justify-center items-center pt-16">
-                <div class="container flex flex-col justify-center items-center px-8 lg:px-12 xl:px-20">
+                <div class="container flex flex-col justify-center items-center px-4 sm:px-8 lg:px-12 xl:px-20">
                     <div class="flex items-center justify-center bg-white rounded-full shadow-md w-60 py-2">
                         <i class="pi pi-circle-fill text-primary" style="font-size: 0.4rem"></i>
                         <p class="text-sm px-2.5 text-gray-600">Visual Storytelling Agency</p>
